968b6703485f6bf24763786cdd413efbf2e6380a
Reddishye <noreply@redactado.es>
2024-05-23 12:11:31 +0200

    screenUtils

--- decoutilities/textUtils/__init__.py
+++ decoutilities/textUtils/__init__.py
@@ -45,6 +45,7 @@ class textUtils:
                 return text
         
     def format(text):
+        import re
         aliases = {
             "{bold}": "\033[1m",
             "{underline}": "\033[4m",
@@ -67,6 +68,9 @@ class textUtils:
 
         for key, value in aliases.items():
             text = text.replace(key, value)
+
+        # Hex {#RRGGBB}
+        text = re.sub(r'{#([0-9A-Fa-f]{6})}', r'\033[38;2;0x\1m', text)
 
         return text + "\033[0m"  # reset at the end
 def format(text):
